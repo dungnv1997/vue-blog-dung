65ad0d711e9d5e447f0c85d393ab5ecb4e566302
dungnv <josephnguyenvandung@gmail.com>
2021-03-22 13:54:07 +0700

    change bloglist

---
+++ components/Blog/BlogList.vue
@@ -0,0 +1,18 @@
+<template>
+   <div>
+      <div class="headerright">
+         <h2>List Blog</h2>
+         <Table  />
+      </div>
+   </div>
+</template>
+<script>
+   import axios from 'axios'
+   import Table from './TableBlog.vue'
+   export default {
+     name: 'admin',
+     components: {
+       Table
+     },   
+   }  
+</script>
